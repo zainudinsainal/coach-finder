cadb7c84f88264323ebc3ba5266f3f41ee01a2ca
zainudinsainal <supernova37@gmail.com>
2021-08-19 12:51:01 +0800

    Filtering coaches

--- app/javascript/src/router/index.js
+++ app/javascript/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 import CoachDetail from '../pages/coaches/CoachDetail.vue'
 import CoachesList from '../pages/coaches/CoachesList.vue'
 import CoachRegistration from '../pages/coaches/CoachRegistration.vue'
