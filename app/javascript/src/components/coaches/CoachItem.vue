26da1bb9a11f899498126b664596ccf6994f0d62
zainudinsainal <supernova37@gmail.com>
2021-08-19 12:02:00 +0800

    Remove turbolink and build coach detail component

--- app/javascript/src/components/coaches/CoachItem.vue
+++ app/javascript/src/components/coaches/CoachItem.vue
@@ -25,10 +25,10 @@ export default {
       return this.firstName + ' ' + this.lastName;
     },
     coachContactLink() {
-      return this.$route.path + this.id + '/contact' // /coaches/c1/contact
+      return this.$route.path + '/' + this.id + '/contact' // /coaches/c1/contact
     },
     coachDetailsLink() {
-      return this.$route.path + this.id; // /coaches/c1
+      return this.$route.path + '/' + this.id; // /coaches/c1
     }
    }
 }
--- app/javascript/src/router/index.js
+++ app/javascript/src/router/index.js
@@ -10,9 +10,14 @@ import NotFound from '../pages/NotFound.vue'
 const routes = [
   { path: '/', redirect: '/coaches' },
   { path: '/coaches', component: CoachesList },
-  { path: '/coaches/:id', component: CoachDetail, children: [
-    { path: 'contact', component: ContactCoach },
-  ] },
+  {
+    path: '/coaches/:id',
+    component: CoachDetail,
+    props: true,
+    children: [
+      { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+    ]
+  },
   { path: '/register', component: CoachRegistration },
   { path: '/requests', component: RequestsReceived },
   { path: '/:notFound(.*)', component: NotFound },
