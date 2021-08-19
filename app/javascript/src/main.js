3efe3401080d1d7c431d57e038ecfe5e888eca0c
zainudinsainal <supernova37@gmail.com>
2021-08-19 11:07:08 +0800

    Adding base components - card, btn & badge

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -2,12 +2,20 @@ import { createApp } from 'vue'
 import router from '@/router'
 import store from './store'
 import App from '@/App.vue'
+import BaseCard from './components/ui/BaseCard.vue'
+import BaseButton from './components/ui/BaseButton.vue'
+import BaseBadge from './components/ui/BaseBadge.vue'
 
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
     const app = createApp(App)
     app.use(store)
     app.use(router)
+
+    app.component('base-card', BaseCard)
+    app.component('base-button', BaseButton)
+    app.component('base-badge', BaseBadge)
+
     app.mount('#app')
   })
 }
